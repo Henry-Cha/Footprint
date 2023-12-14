f67f850728b1e34a1e96bbc24976fc7475964f45
Henry-Cha <sungwon4728@naver.com>
2023-12-14 19:18:16 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/meow/footprint/FootprintApplication.java
@@ -0,0 +1,13 @@
+package com.meow.footprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootprintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meow/footprint/FootprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meow.footprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
