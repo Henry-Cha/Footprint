a45d0976b2748e0d1a6455ee8bfeb9008ab1467c
Henry-Cha <sungwon4728@naver.com>
2023-12-24 17:58:37 +0900

    feat: 스웨거 시큐리티 (jwt 인증) 추가

--- src/main/java/com/meow/footprint/global/config/SwaggerConfig.java
+++ src/main/java/com/meow/footprint/global/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.meow.footprint.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,8 +17,24 @@ public class SwaggerConfig {
                 .title("Footprint")
                 .version("v0.0.1")
                 .description("Footprint API 명세서.");
+
+        SecurityScheme bearer = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("Authorization")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        // Security 요청 설정
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("Authorization");
+
+        Components components = new Components()
+                .addSecuritySchemes("Authorization",bearer);
+
         return new OpenAPI()
-                .components(new Components())
+                .components(components)
+                .addSecurityItem(addSecurityItem)
                 .info(info);
     }
 }
