1a317f1977a62b96bd50ebd063c5fb2a7cadc7ec
Henry-Cha <sungwon4728@naver.com>
2023-12-24 13:41:55 +0900

    feat: modelMapper 추가

---
+++ src/main/java/com/meow/footprint/global/config/MapperConfig.java
@@ -0,0 +1,20 @@
+package com.meow.footprint.global.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper
+                .getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
